45476a34dd8f7fdea6bb17c3c0584d3ee3999f3f
Brycen Harris <143541234+BrycenHarris@users.noreply.github.com>
2024-04-01 15:51:30 -0400

    updated geojp website

--- geojp/geojp.py
+++ geojp/geojp.py
@@ -5,17 +5,50 @@ from ipyleaflet import basemaps
 
 
 class Map(ipyleaflet.Map):
+    """This is the map class that inherits from ipyleaflet.Map.
+
+    Args:
+        ipyleaflet (Map): The ipyleaflet.Map class.
+    """
 
     def __init__(self, center=[20, 0], zoom=2, **kwargs):
+        """Initialize the map.
+
+        Args:
+            center (list, optional): Set the center of the map. Defaults to [20, 0].
+            zoom (int, optional): Set the zoom level of the map. Defaults to 2.
+        """
+
+        if "scroll_wheel_zoom" not in kwargs:
+            kwargs["scroll_wheel_zoom"] = True
+
+        if "add_layer_control" not in kwargs:
+            layer_control_flag = True
+        else:
+            layer_control_flag = kwargs["add_layer_control"]
+        kwargs.pop("add_layer_control", None)
 
         super().__init__(center=center, zoom=zoom, **kwargs)
+        if layer_control_flag:
+            self.add_layers_control()
 
     def add_tile_layer(self, url, name, **kwargs):
         layer = ipyleaflet.TileLayer(url=url, name=name, **kwargs)
         self.add(layer)
 
     def add_basemap(self, name):
+        """
+        Adds a basemap to the current map.
 
+        Args:
+            name (str or object): The name of the basemap as a string, or an object representing the basemap.
+
+        Raises:
+            TypeError: If the name is neither a string nor an object representing a basemap.
+
+        Returns:
+            None
+        """
         if isinstance(name, str):
             url = eval(f"basemaps.{name}").build_url()
             self.add_tile_layer(url, name)
@@ -23,11 +56,20 @@ class Map(ipyleaflet.Map):
             self.add(name)
 
     def add_layers_control(self, position="topright"):
+        """Adds a layers control to the map.
 
+        Args:
+            position (str, optional): The position of the layers control. Defaults to "topright".
+        """
         self.add_control(ipyleaflet.LayersControl(position=position))
 
     def add_geojson(self, data, name="geojson", **kwargs):
- 
+        """Adds a GeoJSON layer to the map.
+
+        Args:
+            data (str | dict): The GeoJSON data as a string or a dictionary.
+            name (str, optional): The name of the layer. Defaults to "geojson".
+        """
         import json
 
         if isinstance(data, str):
@@ -44,7 +86,20 @@ class Map(ipyleaflet.Map):
         self.add(layer)
 
     def add_shp(self, data, name="shp", **kwargs):
+        """
+        Adds a shapefile to the current map.
 
+        Args:
+            data (str or dict): The path to the shapefile as a string, or a dictionary representing the shapefile.
+            name (str, optional): The name of the layer. Defaults to "shp".
+            **kwargs: Arbitrary keyword arguments.
+
+        Raises:
+            TypeError: If the data is neither a string nor a dictionary representing a shapefile.
+
+        Returns:
+            None
+        """
         import shapefile
         import json
 
@@ -54,21 +109,34 @@ class Map(ipyleaflet.Map):
 
         self.add_geojson(data, name, **kwargs)
 
-    def add_raster(self, data, name="raster", **kwargs):
+    def add_image(self, url, bounds, name="image", **kwargs):
+        """Adds an image overlay to the map.
 
-        import rasterio
-        import numpy as np
-
-        if isinstance(data, str):
-            with rasterio.open(data) as src:
-                data = src.read(1)
-                data = np.ma.masked_equal(data, src.nodata)
-                data = data.filled(0)
-
-        if "colormap" not in kwargs:
-            kwargs["colormap"] = ipyleaflet.LinearColormap(
-                colors=["red", "blue", "green"], vmin=0, vmax=255
-            )
-
-        layer = ipyleaflet.ImageOverlay(url=data, name=name, **kwargs)
+        Args:
+            url (str): The URL of the image.
+            bounds (list): The bounds of the image.
+            name (str, optional): The name of the layer. Defaults to "image".
+        """
+        layer = ipyleaflet.ImageOverlay(url=url, bounds=bounds, name=name, **kwargs)
         self.add(layer)
+
+    def add_raster(self, data, name="raster", zoom_to_layer=True, **kwargs):
+        """Adds a raster layer to the map.
+
+        Args:
+            data (str): The path to the raster file.
+            name (str, optional): The name of the layer. Defaults to "raster".
+        """
+
+        try:
+            from localtileserver import TileClient, get_leaflet_tile_layer
+        except ImportError:
+            raise ImportError("Please install the localtileserver package.")
+
+        client = TileClient(data)
+        layer = get_leaflet_tile_layer(client, name=name, **kwargs)
+        self.add(layer)
+
+        if zoom_to_layer:
+            self.center = client.center()
+            self.zoom = client.default_zoom
